210ebc1e6237a2222b6b4dedc52a2c59138db035
krys225 <krys225@gmail.com>
2018-02-22 14:39:06 +0100

    Change tests discovery logic to support more cases
    Change tests discovery logic of AndroidJUnitTestUnitFinder to support
    test cases not annotated with @Test annotation or classes without specified
    test runner

--- pitest/src/main/java/org/pitest/junit/android/AndroidJUnitTestUnitFinder.java
+++ pitest/src/main/java/org/pitest/junit/android/AndroidJUnitTestUnitFinder.java
@@ -8,6 +8,7 @@ import org.pitest.testapi.TestUnit;
 import org.pitest.testapi.TestUnitFinder;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -20,16 +21,26 @@ public class AndroidJUnitTestUnitFinder implements TestUnitFinder {
 
   public static final String ANDROID_JUNIT_CLASS = "android.support.test.runner.AndroidJUnit4";
 
+  private static String[] instrumentedTestsPaths = null;
+
+  static {
+    String instrumentedTestsPathsStr = System.getenv("PITEST_ANDROID_INSTRUMENTED_TESTS_PATH");
+    if (instrumentedTestsPathsStr != null && !instrumentedTestsPathsStr.isEmpty()) {
+      instrumentedTestsPaths = instrumentedTestsPathsStr.split(",");
+    }
+
+  }
+
   @Override
   public List<TestUnit> findTestUnits(Class<?> clazz) {
     final RunWith runWith = clazz.getAnnotation(RunWith.class);
 
     if (runWith == null) {
-      String instrumentedTestsPathsStr = System.getenv("PITEST_ANDROID_INSTRUMENTED_TESTS_PATH");
-      if (instrumentedTestsPathsStr == null || instrumentedTestsPathsStr.isEmpty()) {
+
+      if (instrumentedTestsPaths == null) {
         return Collections.emptyList();
       }
-      String[] instrumentedTestsPaths = instrumentedTestsPathsStr.split(",");
+
       CodeSource classSource = clazz.getProtectionDomain().getCodeSource();
       if (classSource == null || classSource.getLocation() == null) {
         return Collections.emptyList();
@@ -37,7 +48,7 @@ public class AndroidJUnitTestUnitFinder implements TestUnitFinder {
       String classLocation = classSource.getLocation().getPath();
       for (String path : instrumentedTestsPaths) {
         if (classLocation.startsWith(path)) {
-          return getTestsFromClass(clazz);
+          return getTestsFromClass(clazz, false);
         }
       }
       return Collections.emptyList();
@@ -47,7 +58,7 @@ public class AndroidJUnitTestUnitFinder implements TestUnitFinder {
     try {
       runnerClass = runWith.value();
       if (runnerClass.getSimpleName().equals("AndroidJUnit4")) {
-        return getTestsFromClass(clazz);
+        return getTestsFromClass(clazz, true);
       }
       return Collections.emptyList();
 
@@ -55,16 +66,34 @@ public class AndroidJUnitTestUnitFinder implements TestUnitFinder {
       // expected in case of AndroidJUnit4, as it is not in classpath
       // (and probably should not be, since this class is used on device, not locally)
       if (ex.typeName().equals(ANDROID_JUNIT_CLASS)) {
-        return getTestsFromClass(clazz);
+        return getTestsFromClass(clazz, true);
       }
       return Collections.emptyList();
     }
   }
 
-  private List<TestUnit> getTestsFromClass(Class<?> clazz) {
+  // based on android.support.test.internal.runner.TestLoader
+  private boolean isTestMethod(Method m, boolean runnerPresent) {
+    if (m.getAnnotation(Test.class) != null) {
+      return true;
+    }
+
+    if (m.getAnnotations().length != 0) {
+      return false;
+    }
+
+    if (m.getParameterTypes().length == 0 && m.getReturnType().equals(Void.TYPE)
+        && Modifier.isPublic(m.getModifiers())) {
+      return runnerPresent || m.getName().startsWith("test");
+    }
+    return m.getParameterTypes().length == 0 && m.getName().startsWith("test")
+            && m.getReturnType().equals(Void.TYPE);
+  }
+
+  private List<TestUnit> getTestsFromClass(Class<?> clazz, boolean runnerPresent) {
     ArrayList<TestUnit> list = new ArrayList<>();
-    for (Method m : clazz.getMethods()) {
-      if (m.getAnnotation(Test.class) != null) {
+    for (Method m : clazz.getDeclaredMethods()) {
+      if (isTestMethod(m, runnerPresent)) {
         list.add(new AndroidJUnitTestUnit(new Description(m.getName(), clazz)));
       }
     }
